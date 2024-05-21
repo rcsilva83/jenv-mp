ed2a828878f7b8ddc5cf940c0d24ef92a785f859
Rodrigo Carvalho Silva <me@rodsilva.com>
2024-05-21 15:13:35 -0300

    Fixing JENV_HOME

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -2,8 +2,20 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 
 import commands.*
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.toKString
+import okio.Path
+import okio.Path.Companion.toPath
+import platform.posix.getenv
+import utils.isWindows
+import kotlin.experimental.ExperimentalNativeApi
 
-const val JENV_HOME = "~/.jenv"
+@OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
+val userHomeDir: Path =
+    (if (Platform.isWindows()) getenv("USERPROFILE") else getenv("HOME"))!!
+        .toKString().toPath()
+
+val JENV_HOME = userHomeDir.resolve(".jenv")
 
 class Jenv: CliktCommand(name="jenv") {
     override fun run() = Unit
